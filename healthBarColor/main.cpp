a4a88f43fa533488e4dad11a33b841903455f15c
vARMORv <116925504+vARMORv@users.noreply.github.com>
2022-12-14 00:08:41 +0200

    Update main.cpp

--- healthBarColor/main.cpp
+++ healthBarColor/main.cpp
@@ -1,9 +1,8 @@
-﻿#include "main.h"
+#include "main.h"
 #include "Library.hpp"
 
 int healthTopColor = { 0 };
 int healthDownColor = { 0 };
-int CPlayerTags__DrawHealthBar = { 0 };
 
 template <typename T>
 inline void write_memory(std::uintptr_t address, T value, bool protect = true) {
@@ -28,11 +27,11 @@ void __fastcall CPlayerTagHook(void* this_, void* EDX, CVector* possition, float
 		write_memory(SAMP::GetSAMPHandle() + healthTopColor, 0xFF2BBF4A, true);
 		write_memory(SAMP::GetSAMPHandle() + healthDownColor, 0xFF155F25, true);
 	}
-	else if (health > 20 && health < 80){
+	else if (health > 20 && health < 80) {
 		write_memory(SAMP::GetSAMPHandle() + healthTopColor, 0xFFD4C945, true);
 		write_memory(SAMP::GetSAMPHandle() + healthDownColor, 0xFF6A6422, true);
 	}
-	else{
+	else {
 		write_memory(SAMP::GetSAMPHandle() + healthTopColor, 0xFFB4191D, true);
 		write_memory(SAMP::GetSAMPHandle() + healthDownColor, 0xFF5A0C0E, true);
 	}
@@ -41,38 +40,43 @@ void __fastcall CPlayerTagHook(void* this_, void* EDX, CVector* possition, float
 
 void GameloopHooked() {
 	static bool IsInitialized = false;
-	
+
 	if (!IsInitialized && SAMP::IsSAMPInitialized()) {
 		switch (SAMP::GetSAMPVersion()) {
 		case (SAMP::sampVersion::R1): {
-			CPlayerTags__DrawHealthBar = 0x689C0;
 			healthTopColor = 0x68B0C;
 			healthDownColor = 0x68B33;
+			PlayerTags__HealthHook = std::make_unique<memwrapper::memhook<CPlayerTags__Prototype>>(SAMP::GetSAMPHandle() + 0x689C0, &CPlayerTagHook);
+			PlayerTags__HealthHook->install();
 			break;
 		}
 		case (SAMP::sampVersion::R2): {
-			CPlayerTags__DrawHealthBar = 0x68A90;
 			healthTopColor = 0x68BDC;
 			healthDownColor = 0x68C03;
+			PlayerTags__HealthHook = std::make_unique<memwrapper::memhook<CPlayerTags__Prototype>>(SAMP::GetSAMPHandle() + 0x68A90, &CPlayerTagHook);
+			PlayerTags__HealthHook->install();
 			break;
 		}
 		case (SAMP::sampVersion::R3): {
-			CPlayerTags__DrawHealthBar = 0x6C930;
 			healthTopColor = 0x6CA7C;
 			healthDownColor = 0x6CAA3;
+			PlayerTags__HealthHook = std::make_unique<memwrapper::memhook<CPlayerTags__Prototype>>(SAMP::GetSAMPHandle() + 0x6C930, &CPlayerTagHook);
+			PlayerTags__HealthHook->install();
 			break;
 		}
 		case (SAMP::sampVersion::R4): {
-			CPlayerTags__DrawHealthBar = 0x6D060;
 			healthTopColor = 0x6D1AC;
 			healthDownColor = 0x6D1D3;
+			PlayerTags__HealthHook = std::make_unique<memwrapper::memhook<CPlayerTags__Prototype>>(SAMP::GetSAMPHandle() + 0x6D060, &CPlayerTagHook);
+			PlayerTags__HealthHook->install();
 			break;
 
 		}
 		case (SAMP::sampVersion::DL): {
-			CPlayerTags__DrawHealthBar = 0x6CAE0;
 			healthTopColor = 0x6CC2C;
 			healthDownColor = 0x6CC53;
+			PlayerTags__HealthHook = std::make_unique<memwrapper::memhook<CPlayerTags__Prototype>>(SAMP::GetSAMPHandle() + 0x6CAE0, &CPlayerTagHook);
+			PlayerTags__HealthHook->install();
 			break;
 		}
 		default: {
@@ -80,16 +84,11 @@ void GameloopHooked() {
 		}
 		}
 
-                if (CPlayerTags__DrawHealthBar != 0 ){
-                    PlayerTags__HealthHook = std::make_unique<memwrapper::memhook<CPlayerTags__Prototype>>(SAMP::GetSAMPHandle() + CPlayerTags__DrawHealthBar, &CPlayerTagHook);
-                    PlayerTags__HealthHook->install();
-                }
-		
-
 		DrawBarHook = std::make_unique<memwrapper::memhook<CSprite2d__DrawBarChart>>(0x589395, &CDrawHealthBar);
 		DrawBarHook->install();
 		IsInitialized = true;
 	}
+		
 	GameloopHook->call();
 }
 
